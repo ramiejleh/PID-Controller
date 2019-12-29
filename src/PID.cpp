d66e4065ce63696a80d2d3305338b1989b5e76b9
ramiejleh <rami.ejleh1@gmail.com>
2019-12-29 09:05:47 +0000

    Implemented a basic PID Controller

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,21 +10,32 @@ PID::~PID() {}
 
 void PID::Init(double Kp_, double Ki_, double Kd_) {
   /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
+   * Done: Initialize PID coefficients (and errors, if needed)
    */
-
+  
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
+  
+  p_error = 0;
+  i_error = 0;
 }
 
 void PID::UpdateError(double cte) {
   /**
-   * TODO: Update PID errors based on cte.
+   * Done: Update PID errors based on cte.
    */
-
+  // d_error is difference between prev cte (p_error) and the current one
+  d_error = (cte - p_error);
+  // p_error is the previous error
+  p_error = cte;
+  // i_error is the sum of all CTEs until now
+  i_error += cte;
 }
 
 double PID::TotalError() {
   /**
-   * TODO: Calculate and return the total error
+   * Done: Calculate and return the total error
    */
-  return 0.0;  // TODO: Add your total error calc here!
+  return -Kp * p_error - Kd * d_error - Ki * i_error;  // Done: total error
 }
--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,8 @@ int main() {
   /**
    * TODO: Initialize the pid variable.
    */
-
+  pid.Init(0.2, 0.004, 3.0);
+  
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -63,6 +64,12 @@ int main() {
            * NOTE: Feel free to play around with the throttle and speed.
            *   Maybe use another PID controller to control the speed!
            */
+          
+          // Update error values with cte
+          pid.UpdateError(cte);
+          
+          // Calculate steering value (if reasonable error, returns between [-1, 1])
+          steer_value = pid.TotalError();
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
